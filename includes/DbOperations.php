f965dd4539e18ffbf25caf79f02dc066e8467d7d
Itha4 <ithambuthia8@gmail.com>
2020-11-06 22:46:51 +0300

    Update DbOperations.php

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * 
- */
 class DbOperation 
 {
 	private $con;
@@ -14,15 +11,28 @@ class DbOperation
 	}
 
 /*CRUD -> C ->Create*/
-function createUser($username, $password, $email){
-	$password = md5($pass);
-	$stmt = $this->con->prepare("INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, '?', '?', '?');");
-	$stmt->bind_param("sss",$username,$password,$email);
-	if($stmt->execute()){
-		return true;
+public function createUser($username, $pass, $email){
+	if($this->isUserExist($username,$email)){
+		return 0;
 	}else{
-		return false;
+
+			$password = md5($pass);
+			$stmt = $this->con->prepare("INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES (NULL, '?', '?', '?');");
+			$stmt->bind_param("sss",$username,$password,$email);
+			if($stmt->execute()){
+				return 1;
+			}else{
+			return 2;
+	}
 	}
 
+}
+
+private function isUserExist($username,$email){
+	$stmt = $this->con->prepare("SELECT id FROM users WHERE username = ? OR email * ? ");
+	$stmt ->bind_param("$$", $username,$email);
+	$stmt ->execute();
+	$stmt ->store_result();
+	return $stmt ->num_rows >0;
 }
 }
