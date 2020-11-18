61df4d499b3a04ac1cbdcae39045558fe26edb14
Itha4 <ithambuthia8@gmail.com>
2020-11-18 22:16:38 +0300

    Update userLogin.php

--- v1/userLogin.php
+++ v1/userLogin.php
@@ -1,3 +1,31 @@
 <?php
 require_once '../includes/DbOperations.php';
+
 $response = array();
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+	if(
+		isset()$_POST['username'])and
+		isset($_POST['email'])and
+			isset($_POST['password'])){
+	$db = new DbOperations();
+	$result = $db->createUser(	$_POST['username'],
+								$_POST['email'],
+								$_POST['password']
+							);
+
+	if($result == 1){
+		$response['error'] = false;
+		$response['message'] = "User registered successfully";
+	}elseif($result == 2){
+		$response['error'] = true;
+		$response['message'] = "Error occurred,try again";
+	}
+}else{
+	$response['error'] = true;
+		$response['message'] = "Required fields are missing";
+}else{
+	$response['error'] = true;
+		$response['message'] = "Invalid Request";
+}
+}
+}
